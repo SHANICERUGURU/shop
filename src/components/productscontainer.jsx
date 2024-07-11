4cdd3a8984f2462192510cab8c6bfd9c25ed8be8
SHANICERUGURU <rugurushanice990@gmail.com>
2024-07-11 08:54:10 +0300

    push changes

--- src/components/productscontainer.jsx
+++ src/components/productscontainer.jsx
@@ -1,9 +1,11 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import ProductCard from "./productcard";
 import NavBar from "./navbar";
 const ProductsContainer=()=>{
     const [products,setProducts]=useState([])
-
+    const searchInputRef=useRef("")
+    const [searchQuery,setSearchQuery]=useState("")
+    
     useEffect(()=>{
         fetch("https://dummyjson.com/products").then((response)=>{
             response.json().then((resp)=>{
@@ -14,20 +16,41 @@ const ProductsContainer=()=>{
             console.error(err)
         })
     },[])
+    useEffect(() => {
+        if (searchInputRef.current) {
+            searchInputRef.current.value = "";
+        }
+    }, []);
+
+    const filteredProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
     return(
         <>
         <NavBar/>
+        <input
+                type="text"
+                placeholder="Search product..."
+                onChange={(e) => setSearchQuery(e.target.value)}
+                ref={searchInputRef}
+            />
         <h1>BUY NOW!!!</h1>
         <div className="container">
            <div className="row justify-content-around">
            {
-            products.map((prod,index)=>{
+           
+            
+           filteredProducts.length>0?(
+            filteredProducts.map((prod,index)=>{
                 return(
                     <div className="col-md-4 mb-4" key={index}>
                       <ProductCard product={prod}/>
                     </div>
                    )
             })
+        ):(
+            <h1>No products found!!</h1>
+        )
            }
            </div>
         </div>
